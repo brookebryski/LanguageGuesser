989ea8d66eb6800920eda7d688dc8531ed3038e6
BrookeBryski <brookeabryski@gmail.com>
2023-06-21 13:01:25 -0700

    finished

---
+++ index.js
@@ -0,0 +1,12 @@
+const franc = require("franc");
+const langs = require("langs");
+const input = process.argv[2];
+const langCode = franc(input);
+
+if (langCode === 'und') {
+    console.log("Could not find that language code.")
+} else {
+    const language = langs.where("3", langCode);
+    console.log(`Best guess is: ${language.name}`);
+}
+
